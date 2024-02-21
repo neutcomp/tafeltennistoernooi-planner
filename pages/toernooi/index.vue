8464759cb9fc03b10e9700352f6d3a40be7207d8
Bjorn van der Neut <bvanderneut@independer.nl>
2024-02-21 16:43:03 +0100

    Create Tournament component

--- pages/toernooi/index.vue
+++ pages/toernooi/index.vue
@@ -5,34 +5,7 @@
         <h1>Overzicht toernooien</h1>
       </div>
 
-      <button @click="isOpen = true" class="btn" type="button">Toernooi toevoegen</button>
-
-      <div v-show="isOpen" class="absolute inset-0 flex items-center justify-center bg-gray-700 bg-opacity-50">
-        <div class="max-w-2xl p-6 bg-white rounded-md shadow-xl">
-          <div class="flex items-center justify-between">
-            <form class="max-w-sm mx-auto">
-              <div class="mb-3">
-                <label for="name">Naam</label>
-                <input
-                  type="input"
-                  id="name"
-                  v-model="name"
-                  class="w-96"
-                  placeholder="Jeugd clubkampioenschappen 2024"
-                  required
-                />
-              </div>
-              <div id="error" class="error">{{ errorMessage }}</div>
-            </form>
-          </div>
-          <div class="mt-4">
-            <button @click="isOpen = false" class="px-6 py-2 text-blue-800 border border-blue-600 rounded">
-              Cancel
-            </button>
-            <button type="submit" @click.prevent="addTournament(name)" class="btn">Voeg toernooi toe</button>
-          </div>
-        </div>
-      </div>
+      <Tournament />
 
       <div class="mt-4">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -64,11 +37,9 @@
 </template>
 
 <script setup>
-const isOpen = ref(false);
-let errorMessage = ref(null);
-const tournaments = await getTournaments();
-const name = ref(null);
+let tournaments = await getTournaments();
 
+// Below functions should move to the component Tournament.vue
 async function getTournaments() {
   return await $fetch('/api/tournament');
 }
@@ -84,44 +55,6 @@ async function deleteTournament(id) {
       });
     } catch (error) {
       errorMessage = error.message;
-      return;
-    }
-
-    this.tournaments = await getTournaments();
-  }
-}
-
-async function addTournament(name) {
-  if (name) {
-    try {
-      await $fetch('/api/tournament/add', {
-        method: 'POST',
-        body: {
-          userId: 1, // Todo make this depending on the user that has login
-          name: name,
-        },
-      });
-    } catch (error) {
-      this.errorMessage = error.message;
-      return;
-    }
-
-    this.tournaments = await getTournaments();
-  }
-}
-
-async function editTournament(editedUser) {
-  if (editedUser.name) {
-    try {
-      await $fetch('/api/tournament', {
-        method: 'PUT',
-        body: {
-          userId: 1,
-          name: name,
-        },
-      });
-    } catch (error) {
-      this.errorMessage = error.message;
       return;
     }
 
