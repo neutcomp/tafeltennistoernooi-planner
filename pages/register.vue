f3e045870fa25618bfc134ebfaad09852f4b0e8d
Bjorn van der Neut <bjornvdneut+github@gmail.com>
2024-02-22 21:18:57 +0100

    Add login and register pages

---
+++ pages/register.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup></script>
+<template>
+    <div>
+        <h1 class="mb-4 text-xl font-bold">Register</h1>
+        <form>
+            <input class="w-full border p-2 rounded-lg mb-4" type="email" placeholder="Email" />
+            <input class="w-full border p-2 rounded-lg mb-4" type="text" placeholder="Username" />
+            <input class="w-full border p-2 rounded-lg mb-4" type="password" placeholder="Password" />
+            <button type="submit"
+                class="bg-blue-500 hover:bg-blue-600 transition-all duration-200 w-full text-blue-50 rounded-lg p-2">
+                Register
+            </button>
+        </form>
+    </div>
+</template>
+
+<style></style>
