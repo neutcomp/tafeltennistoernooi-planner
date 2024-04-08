3e4a95b1aaf9e9ac42758b572bbb4a0f5dbc81e7
Bjorn van der Neut <bjornvdneut+github@gmail.com>
2024-04-08 21:08:53 +0200

    Cleanup helper

--- server/utils/helper.ts
+++ server/utils/helper.ts
@@ -3,25 +3,8 @@ import { H3Event } from 'h3';
 import { getServerSession } from '#auth'
 import prisma from '../../db/db';
 
-export const UserSchema = Joi.object({
-    firstname: Joi.string().max(20).min(2).required(),
-    lastname: Joi.string().max(30).min(2).required(),
-    email: Joi.string().max(50).min(6).required().email()
-})
-
-export const UserUpdateSchema = Joi.object({
-    id: Joi.number().required(),
-    firstname: Joi.string().max(20).min(2).required(),
-    lastname: Joi.string().max(30).min(2).required(),
-    email: Joi.string().max(50).min(6).required().email()
-})
-
 export const UserDeleteSchema = Joi.object({
     id: Joi.string().required()
-})
-
-export const LoginSchema = Joi.object({
-    email: Joi.string().max(50).min(6).required().email()
 })
 
 export const AttendeeSchema = Joi.object({
@@ -42,19 +25,11 @@ export const TournamentDeleteSchema = Joi.object({
     id: Joi.number().required()
 })
 
-export const cookieName = 'isAuthenticated'
-
-export const checkAuthentication = (event: H3Event) => {
-    return Boolean(event.context?.user);
-}
-
 export const getTokenId = async (event: H3Event) => {
     const session = await getServerSession(event);
 
     // If not authenticated do nothing
     if (session) {
-
-
         // Get the JWT token from the DB
         const token = await prisma.user.findFirst({
             where: {
