03675c6ed2f2e0356bd929e905fa5e8f073303c8
Bjorn van der Neut <bjornvdneut+github@gmail.com>
2024-04-13 20:17:17 +0200

    Add multiselect to select attendees

--- pages/toernooi/[id].vue
+++ pages/toernooi/[id].vue
@@ -1,19 +1,61 @@
-<template>
-    <div class="container mx-auto">
-        <div class="justify-center">
-            <div>
-                <h1>Toernooi deelnemers</h1>
-                <NuxtLink to="/toernooi/" class="btn">terug</NuxtLink>
+<script lang="ts">
+definePageMeta({
+    middleware: 'auth'
+})
 
-                {{ id }}
-            </div>
-        </div>
-    </div>
-</template>
+export default {
+    name: 'tournamentAttendees',
+    data() {
+        return {
+            attendees: [{
+                id: '',
+                firstname: '',
+                lastname: '',
+                rating: ''
+            }],
+            selectedAttendees: [],
+        }
+    },
+    async mounted() {
+        await this.getAttendees();
+    },
+    methods: {
+        async getAttendees() {
+            const { data } = await useFetch('/api/attendee');
 
-<script type="ts" setup>
-const route = useRoute()
-const id = route.params.id
-// When accessing /posts/1, route.params.id will be 1
-console.log(route.params.id)
+            if (data) {
+                this.attendees = data;
+            }
+        },
+        async addAttendee() {
+            console.log(this.selectedAttendees)
+            // try {
+            //     await useFetch('/api/attendee/add-attendees', {
+            //         method: 'POST',
+            //         body: this.selectedAttendees
+            //     });
+            // } catch (error: any) {
+            //     this.errorMessage = error.message;
+            //     return;
+            // }
+        }
+    }
+}
 </script>
+
+<template>
+    <form class="max-w-sm mx-auto">
+        <USelectMenu value-attribute="id" option-attribute="firstname" v-model="selectedAttendees" :options="attendees"
+            multiple searchable placeholder="Select people">
+            <template #option="{ option: attendee }">
+                <span class="truncate">{{ attendee?.firstname }} {{ attendee?.lastname }}</span>
+            </template>
+
+        </USelectMenu>
+
+        <button @click.prevent="addAttendee()" class="btn" type="button">Deelnemers
+            toevoegen</button>
+    </form>
+
+    {{ selectedAttendees }}
+</template>
